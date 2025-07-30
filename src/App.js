5ee0897ee729e1771af4cfd8145290f817a6b2f4
Shakti1356 <shaktiranjan2567@gmail.com>
2025-07-30 20:34:12 +0530

    Add src/App.js 1753887851056

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Transfer from './pages/Transfer';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transfer" element={<Transfer />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
